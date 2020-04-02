3c4385f51c8674c6f85aed84dc4b152384f93571
qwerty2501 <qwerty2501@live.jp>
2020-04-03 00:51:48 +0900

    WIP implemented calculate.

--- crate/src/calculator.rs
+++ crate/src/calculator.rs
@@ -1,26 +1,52 @@
+use crate::error::*;
 use crate::parser::*;
 use crate::result::Result;
+use bigdecimal::*;
 
-struct Calculator {
-    status: Status,
+pub fn calculate(input: &str) -> Result<String> {
+    Ok(calculate_tokens(parse(input)?)?.to_string())
 }
 
-enum Status {
-    Empty,
-}
-
-impl Calculator {
-    fn calc(&mut self, input: &str) -> Result<String> {
-        match self.status {
-            _ => Ok(input.to_string()),
+fn calculate_tokens(tokens: Vec<Token>) -> Result<BigDecimal> {
+    let tokens = operate_tokens(tokens, Operator::Division)?;
+    let tokens = operate_tokens(tokens, Operator::Multiplication)?;
+    let tokens = operate_tokens(tokens, Operator::Plus)?;
+    let mut tokens = operate_tokens(tokens, Operator::Minus)?;
+    if tokens.len() == 1 {
+        let token = tokens.pop().unwrap();
+        if let Token::Decimal(decimal) = token {
+            Ok(decimal)
+        } else {
+            Err(AppError::InvalidExpression)
         }
+    } else {
+        Err(AppError::InvalidExpression)
     }
 }
 
-pub fn calculate(input: &str) -> Result<String> {
-    let _ = parse(input);
-    let mut calculator = Calculator {
-        status: Status::Empty,
-    };
-    calculator.calc(input)
+fn operate_tokens(tokens: Vec<Token>, op: Operator) -> Result<Vec<Token>> {
+    let mut result_tokens = Vec::with_capacity(tokens.len());
+    for (index, token) in tokens.iter().enumerate() {
+        match token {
+            Token::Decimal(_) => result_tokens.push(token.clone()),
+            Token::Operator(iop) => {
+                if iop == &op {
+                    let v1 = if let Some(Token::Decimal(decimal)) = result_tokens.pop() {
+                        Ok(decimal)
+                    } else {
+                        Err(AppError::InvalidExpression)
+                    }?;
+                    let v2 = if let Some(Token::Decimal(decimal)) = tokens.get(index + 1) {
+                        Ok(decimal)
+                    } else {
+                        Err(AppError::InvalidExpression)
+                    }?;
+                    result_tokens.push(Token::Decimal(op.operate(&v1, v2)));
+                } else {
+                    result_tokens.push(token.clone());
+                }
+            }
+        }
+    }
+    Ok(result_tokens)
 }
--- crate/src/parser.rs
+++ crate/src/parser.rs
@@ -3,12 +3,12 @@ use crate::result::Result;
 use bigdecimal::*;
 use std::str::FromStr;
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Token {
     Decimal(BigDecimal),
     Operator(Operator),
 }
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Operator {
     Plus,
     Minus,
@@ -86,6 +86,17 @@ impl Parser {
     }
 }
 
+impl Operator {
+    pub fn operate(&self, v1: &BigDecimal, v2: &BigDecimal) -> BigDecimal {
+        match self {
+            Operator::Plus => v1 + v2,
+            Operator::Minus => v1 - v2,
+            Operator::Division => v1 / v2,
+            Operator::Multiplication => v1 * v2,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -103,10 +114,18 @@ mod tests {
     #[test_case("-3.343",vec![Token::Decimal(BigDecimal::from(-3.343))]=> Ok(()))]
     #[test_case("-a3.343",vec![Token::Decimal(BigDecimal::from(-3.343))]=> Err(AppError::InvalidChar('a')))]
     #[test_case("4000%20",vec![] => Err(AppError::InvalidExpression))]
-    #[test_case("(400+20)*20",vec![] => Err(AppError::InvalidChar('(')))]
+    #[test_case("\u{0028}400+20\u{0029}*20",vec![] => Err(AppError::InvalidChar('\u{0028}')))]
     #[test_case("400 + 23",vec![Token::Decimal(BigDecimal::from(400)),Token::Operator(Operator::Plus),Token::Decimal(BigDecimal::from(23))] => Ok(()))]
     fn parse_works(input: &str, expected: Vec<Token>) -> Result<()> {
         assert_eq!(expected, parse(input)?);
         Ok(())
+    }
+
+    #[test_case(Operator::Plus,BigDecimal::from(100),BigDecimal::from(300) => BigDecimal::from(400))]
+    #[test_case(Operator::Minus,BigDecimal::from(300),BigDecimal::from(500) => BigDecimal::from(200))]
+    #[test_case(Operator::Multiplication,BigDecimal::from(300),BigDecimal::from(500) => BigDecimal::from(1500))]
+    #[test_case(Operator::Division,BigDecimal::from(500),BigDecimal::from(100) => BigDecimal::from(5))]
+    fn operate_works(op: Operator, v1: BigDecimal, v2: BigDecimal) -> BigDecimal {
+        op.operate(&v1, &v2)
     }
 }
